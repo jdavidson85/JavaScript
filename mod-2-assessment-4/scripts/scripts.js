ce655aec083b3200f1a94e88fc92e2b29b2a18be
Joe Davidson <79029363+jdavidson85@users.noreply.github.com>
2023-06-11 20:28:02 -0500

    Add files via upload

---
+++ mod-2-assessment-4/scripts/scripts.js
@@ -0,0 +1,30 @@
+/*var a = Math.floor(Math.random() * (999)) + 1;
+
+while (true) {
+
+  var b = prompt("Enter a number in between 1 and 1000");
+
+  if (b == a) {
+    alert("You're right!");
+    break; // Stops loop
+  } else if (!b) {
+    alert("The Correct Answer was: " + a); //Shows correct answer
+    break;
+  } else {
+    alert("Incorrect! Try again!");
+  }
+}
+var a = 432;
+
+while (true) {
+
+   var b = prompt("Enter a number in between 1 and 1000");
+
+   if (b == a){
+        alert("You're right!");
+        break; // Stops loop
+    } else if (!b) { break; } 
+    else {
+        alert("Incorrect! Try again!");
+    }
+}*/
